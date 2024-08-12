86080184506ac72420feba7a2a774692e9b11bfa
bowd <dumbogdan@gmail.com>
2024-08-12 21:33:13 +0200

    fix: make variables private and of the same size

--- src/ImmutableEncoder.sol
+++ src/ImmutableEncoder.sol
@@ -4,19 +4,17 @@ pragma solidity ^0.8;
 abstract contract ImmutableEncoder {
     error DataTooLarge(uint256 available, uint256 requested);
 
-    uint256 private immutable storedDataBytesLength;
-    uint8 immutable slotsCount;
-    uint32 immutable availableDataBytesLength;
+    uint256 private immutable storedDataBytes;
+    uint256 private immutable slotsCount;
+    uint256 private immutable availableDataBytes;
 
     constructor(uint8 _slotsCount, uint256 dataLength) {
         slotsCount = _slotsCount;
-        availableDataBytesLength = uint32(slotsCount) * 32;
-        storedDataBytesLength = dataLength;
-        if (storedDataBytesLength > availableDataBytesLength) {
-            revert DataTooLarge(
-                availableDataBytesLength,
-                storedDataBytesLength
-            );
+        availableDataBytes = slotsCount * 32;
+
+        storedDataBytes = dataLength;
+        if (storedDataBytes > availableDataBytes) {
+            revert DataTooLarge(availableDataBytes, storedDataBytes);
         }
     }
 
@@ -29,25 +27,21 @@ abstract contract ImmutableEncoder {
     function bytesToBytes32Array(
         bytes memory data
     ) internal view returns (bytes32[] memory slots) {
-        uint256 _slotsCount = uint256(slotsCount);
+        uint256 _slotsCount = slotsCount;
         assembly {
             mstore(data, _slotsCount)
             slots := data
         }
-        return slots;
     }
 
     function bytes32ArrayToBytes(
         bytes32[] memory slots
     ) internal view returns (bytes memory data) {
         assert(slots.length == slotsCount);
-        uint256 _storedDataBytesLength = storedDataBytesLength;
-
+        uint256 _storedDataBytes = storedDataBytes;
         assembly {
-            mstore(slots, _storedDataBytesLength)
+            mstore(slots, _storedDataBytes)
             data := slots
         }
-
-        return data;
     }
 }
